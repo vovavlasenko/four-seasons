bcaef5b5dc8d16d35a40075ea16f417e492bcae6
vovavlasenko <voejer2007@ya.ru>
2024-07-04 21:52:19 +0300

    Created Card and CardComparator classes, added DOTween

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,90 @@
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+using DG.Tweening;
+
+public class Card : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Sprite _faceSprite;
+    [SerializeField] private Sprite _backSprite;
+    [SerializeField] private Image _cardImage;
+
+    public static event Action<Card> SelectCard;
+
+    private bool _canRotate = true;
+    private bool _pairWasFound = false;
+
+    public Sprite FaceSprite { get => _faceSprite; }
+
+    private void Start()
+    {
+        _cardImage.sprite = _backSprite;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (_canRotate && !_pairWasFound)
+        {
+            StartRevealingCard();
+        }
+    }
+
+    private void StartRevealingCard()
+    {
+        _canRotate = false;
+        CardFlipWithCallback(90f, FinishRevealingCard);
+    }
+
+    private void CardFlipWithCallback(float angle, Action callback)
+    {
+        Tween t = _cardImage.transform.DORotate(new Vector3(0f, angle, 0f), 0.25f);
+        t.OnComplete(() => callback());
+    }
+
+    private void FinishRevealingCard()
+    {
+        _cardImage.sprite = _faceSprite;
+        CardFlipWithCallback(180f, OnCardSelected);
+    }
+
+    public void StartUnrevealingCard()
+    {
+        CardFlipWithCallback(90f, FinishUnrevealingCard);
+    }
+
+    private void FinishUnrevealingCard()
+    {
+        _cardImage.sprite = _backSprite;
+        CardFlipWithCallback(0f, EnableRotating);
+    }
+
+    private void EnableRotating()
+    {
+        _canRotate = true;
+    }
+
+    private void OnCardSelected()
+    {
+        SelectCard?.Invoke(this);
+    }
+
+    public void OnPairFound()
+    {
+        _canRotate = false;
+        _pairWasFound = true;
+        _cardImage.transform.DOScale(0, 0.5f);
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+       // transform.localScale = new Vector3(1.1f, 1.1f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+       // transform.localScale = new Vector3(1f, 1f);
+    }
+}
---
+++ Assets/Scripts/CardComparator.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class CardComparator : MonoBehaviour
+{
+    private Card _firstCard;
+    private Card _secondCard;
+
+    private void OnEnable()
+    {
+        Card.SelectCard += OnCardSelect;
+    }
+
+    private void OnDisable()
+    {
+        Card.SelectCard -= OnCardSelect;
+    }
+
+    public void OnCardSelect(Card card)
+    {
+        if (_firstCard == null)
+        {
+            _firstCard = card;
+        }
+
+        else
+        {
+            _secondCard = card;
+            CompareCards();
+            _firstCard = null;
+        }
+    }
+
+    private void CompareCards()
+    {
+        if (_firstCard.FaceSprite == _secondCard.FaceSprite)
+        {
+            _firstCard.OnPairFound();
+            _secondCard.OnPairFound();
+        }
+
+        else
+        {
+            _firstCard.StartUnrevealingCard();
+            _secondCard.StartUnrevealingCard();
+        }
+    }
+}
